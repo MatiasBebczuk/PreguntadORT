79a0e626501445fc45dc763433db5ce07c9ea9ae
MatiasBebczuk <48242936@est.ort.edu.ar>
2024-08-29 15:24:00 -0300

    models, respuestas.cs

---
+++ Models/Respuestas.cs
@@ -0,0 +1,9 @@
+public class Respuestas
+{       
+        public int idRespuesta { get; set; }
+        public int Opcion { get; set; }
+        public string Contenido {get; set;}
+        public bool Correcta { get; set; }
+        public string Foto {get; set;}
+
+}
